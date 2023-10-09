3fe9dbe049eed94bb906850611dcd76325c21671
scilightener <45340222+scilightener@users.noreply.github.com>
2023-10-09 08:27:13 +0000

    Copilot third commit-2

--- comments.js
+++ comments.js
@@ -1,70 +1,54 @@
 // Create web server
-// ===========================
-
-// Import modules
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const Comment = require('../models/comment');
-const Post = require('../models/post');
-const User = require('../models/user');
-const cors = require('./cors');
-const authenticate = require('../authenticate');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+const commentFilePath = 'comments.json';
 
-// Use bodyParser
+// Parse JSON body
 app.use(bodyParser.json());
 
-// Create router
-const router = express.Router();
+// Read comments from file
+// If the file does not exist, create it with an empty array
+let comments = [];
+try {
+  comments = JSON.parse(fs.readFileSync(commentFilePath, 'utf8'));
+} catch (err) {
+  if (err.code === 'ENOENT') {
+    fs.writeFileSync(commentFilePath, '[]', 'utf8');
+  } else {
+    throw err;
+  }
+}
 
-// Set up router
-router.route('/')
-.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
-.get(cors.cors, (req, res, next) => {
-    Comment.find(req.query)
-    .populate('author')
-    .then((comments) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(comments);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-})
-.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    if (req.body != null) {
-        Comment.create(req.body)
-        .then((comment) => {
-            Comment.findById(comment._id)
-            .populate('author')
-            .then((comment) => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(comment);
-            })
-        }, (err) => next(err))
-        .catch((err) => next(err));
-    }
-    else {
-        err = new Error('Comment not found in request body');
-        err.statusCode = 404;
-        return next(err);
-    }
-})
-.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /comments');
-})
-.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    Comment.deleteMany({})
-    .then((response) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(response);
-    }, (err) => next(err))
-    .catch((err) => next(err));
+// Write comments to file
+function writeComments(comments) {
+  fs.writeFileSync(commentFilePath, JSON.stringify(comments), 'utf8');
+}
+
+// Serve static files
+app.use(express.static('public'));
+
+// Get comments
+app.get('/comments', (req, res) => {
+  res.json(comments);
 });
 
-router.route('/:commentId')
-.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
-.get(cors.cors, (req, res, next
+// Add comment
+app.post('/comments', (req, res) => {
+  const { name, comment } = req.body;
+  if (!name || !comment) {
+    res.status(400).send('Missing name or comment');
+  } else {
+    const newComment = { name, comment };
+    comments.push(newComment);
+    writeComments(comments);
+    res.json(newComment);
+  }
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
